3ffb499a4f0e9beff10f4ab29335be38c095df20
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2025-08-03 12:24:06 -0500

    fix for springBoot requiring a / route

--- src/main/java/com/portfolio/templateService/TemplateController.java
+++ src/main/java/com/portfolio/templateService/TemplateController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TemplateController {
 
+    @GetMapping("/")
+    public String sayNothing() {
+        return "Nothing!";
+    }
+
     @GetMapping("/template")
     public String sayHello() {
         return "Hello, World!";
